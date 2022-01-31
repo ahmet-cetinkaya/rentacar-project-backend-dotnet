ece6b57d3f4dd6dd48a082916e980fff2f5c2f0d
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-01-31 17:31:02 +0300

    🎉 Initial Commit

---
+++ src/rentACar/Domain/Entities/Car.cs
@@ -0,0 +1,27 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Car : Entity
+{
+    public int ModelId { get; set; }
+    public int ColorId { get; set; }
+    public string Plate { get; set; }
+    public short ModelYear { get; set; }
+
+    public virtual Color Color { get; set; }
+    public virtual Model Model { get; set; }
+
+    public Car()
+    {
+    }
+
+    public Car(int id, int modelId, int colorId, string plate, short modelYear) : this()
+    {
+        Id = id;
+        ModelId = modelId;
+        ColorId = colorId;
+        Plate = plate;
+        ModelYear = modelYear;
+    }
+}
---
+++ src/rentACar/Domain/Entities/Model.cs
@@ -0,0 +1,33 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Model : Entity
+{
+    public string Name { get; set; }
+    public decimal DailyPrice { get; set; }
+    public int BrandId { get; set; }
+    public int TransmissionId { get; set; }
+    public int FuelId { get; set; }
+    public string ImageUrl { get; set; }
+
+    public virtual Brand Brand { get; set; }
+    public virtual Transmission Transmission { get; set; }
+    public virtual Fuel Fuel { get; set; }
+
+    public Model()
+    {
+    }
+
+    public Model(int id, string name, decimal dailyPrice, int brandId, int transmissionId, int fuelId,
+        string imageUrl) : this()
+    {
+        Id = id;
+        Name = name;
+        DailyPrice = dailyPrice;
+        BrandId = brandId;
+        TransmissionId = transmissionId;
+        FuelId = fuelId;
+        ImageUrl = imageUrl;
+    }
+}
