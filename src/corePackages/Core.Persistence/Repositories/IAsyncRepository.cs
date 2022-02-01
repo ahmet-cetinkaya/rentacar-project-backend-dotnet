1fe3eb034c4fbb9322dd347202301a64f1fa1456
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-01 12:24:19 +0300

    ✨ Repository

---
+++ src/corePackages/Core.Persistence/Repositories/IAsyncRepository.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq.Expressions;
+using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Core.Persistence.Repositories;
+
+public interface IAsyncRepository<T> where T : Entity
+{
+    Task<T?> GetAsync(Expression<Func<T, bool>> predicate);
+
+    Task<IPaginate<T>> GetListAsync(Expression<Func<T, bool>>? predicate = null,
+                                    Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                                    Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+                                    int index = 0,
+                                    int size = 10,
+                                    bool enableTracking = true,
+                                    CancellationToken cancellationToken = default);
+
+    IQueryable<T> Query();
+    Task<T> AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(T entity);
+}
---
+++ src/corePackages/Core.Persistence/Repositories/IRepository.cs
@@ -0,0 +1,22 @@
+﻿using System.Linq.Expressions;
+using Core.Persistence.Paging;
+using Microsoft.EntityFrameworkCore.Query;
+
+namespace Core.Persistence.Repositories;
+
+public interface IRepository<T> where T : Entity
+{
+    T Get(Expression<Func<T, bool>> predicate);
+
+    IPaginate<T> GetList(Expression<Func<T, bool>>? predicate = null,
+                         Func<IQueryable<T>, IOrderedQueryable<T>>? orderBy = null,
+                         Func<IQueryable<T>, IIncludableQueryable<T, object>>? include = null,
+                         int index = 0,
+                         int size = 10,
+                         bool enableTracking = true);
+
+    IQueryable<T> Query();
+    T Add(T entity);
+    void Update(T entity);
+    void Delete(T entity);
+}
