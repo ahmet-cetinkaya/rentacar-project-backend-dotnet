ae20ae305de491e40a205d7b485e0258c69332d6
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-02 15:14:42 +0300

    🗃️ Create Migrations

--- src/rentACar/WebAPI/Program.cs
+++ src/rentACar/WebAPI/Program.cs
@@ -1,13 +1,20 @@
-var builder = WebApplication.CreateBuilder(args);
+using Application;
+using Persistence;
+
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
+builder.Services.AddApplicationServices();
+builder.Services.AddPersistenceServices(builder.Configuration);
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+WebApplication app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
