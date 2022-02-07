ba8acc2ec2d649d10686cddaac1df78012f44175
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-07 19:17:35 +0300

    🔧(infrastructure): Add IFindeksCreditRateService injection

---
+++ src/rentACar/Infrastructure/InfrastructureServiceRegistration.cs
@@ -0,0 +1,14 @@
+﻿using Application.Services;
+using Infrastructure.Adapters.FakeFindeksCreditRateService;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Infrastructure;
+
+public static class InfrastructureServiceRegistration
+{
+    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services)
+    {
+        services.AddScoped<IFindeksCreditRateService, FakeFindeksCreditRateServiceAdapter>();
+        return services;
+    }
+}
