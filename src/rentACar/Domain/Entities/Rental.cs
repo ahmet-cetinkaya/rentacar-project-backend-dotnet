d1850758d811a4c7c7e9bb77d88dd38213ea31ab
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-07 11:57:32 +0300

    🏷️(domain, entities): Update Rental

--- src/rentACar/Domain/Entities/Rental.cs
+++ src/rentACar/Domain/Entities/Rental.cs
@@ -6,20 +6,22 @@ public class Rental : Entity
 {
     public int CarId { get; set; }
 
-    //public int CustomerId { get; set; }
+    public int CustomerId { get; set; }
     public DateTime RentStartDate { get; set; }
     public DateTime RentEndDate { get; set; }
     public DateTime? ReturnDate { get; set; }
 
-    public virtual Car? Car { get; set; }
+    public virtual Car Car { get; set; }
+    public virtual Customer Customer { get; set; }
 
     public Rental()
     {
     }
 
-    public Rental(int id, int carId, DateTime rentStartDate, DateTime rentEndDate, DateTime? returnDate)
+    public Rental(int id, int customerId, int carId, DateTime rentStartDate, DateTime rentEndDate, DateTime? returnDate)
     {
         Id = id;
+        CustomerId = customerId;
         CarId = carId;
         RentStartDate = rentStartDate;
         RentEndDate = rentEndDate;
