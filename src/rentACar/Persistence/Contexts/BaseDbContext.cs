45588b0631daf018798796b6787352b1d7ef55ac
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-07 11:58:10 +0300

    🗃️(persistence, contexts): Update BaseDbContext
    Add Customers
    Add CorporateCustomers
    Add IndividualCustomers
    Add Customers seed
    Add CorporateCustomers seed
    Add IndividualCustomers seed
    Update Rentals seed

--- src/rentACar/Persistence/Contexts/BaseDbContext.cs
+++ src/rentACar/Persistence/Contexts/BaseDbContext.cs
@@ -11,7 +11,10 @@ public class BaseDbContext : DbContext
     public DbSet<Brand> Brands { get; set; }
     public DbSet<Car> Cars { get; set; }
     public DbSet<Color> Colors { get; set; }
+    public DbSet<CorporateCustomer> CorporateCustomers { get; set; }
+    public DbSet<Customer> Customers { get; set; }
     public DbSet<Fuel> Fuel { get; set; }
+    public DbSet<IndividualCustomer> IndividualCustomers { get; set; }
     public DbSet<Model> Models { get; set; }
     public DbSet<Transmission> Transmissions { get; set; }
 
@@ -57,13 +60,46 @@ public class BaseDbContext : DbContext
             c.Property(p => p.Name).HasColumnName("Name");
             c.HasMany(p => p.Cars);
         });
+
+        modelBuilder.Entity<CorporateCustomer>(c =>
+        {
+            c.ToTable("CorporateCustomers").HasKey(c => c.Id);
+            c.Property(c => c.Id).HasColumnName("Id");
+            c.Property(c => c.CustomerId).HasColumnName("CustomerId");
+            c.Property(c => c.CompanyName).HasColumnName("CompanyName");
+            c.Property(c => c.TaxNo).HasColumnName("TaxNo");
+            c.HasOne(c => c.Customer);
+        });
+
+        modelBuilder.Entity<Customer>(c =>
+        {
+            c.ToTable("Customers").HasKey(c => c.Id);
+            c.Property(c => c.Id).HasColumnName("Id");
+            c.Property(c => c.Email).HasColumnName("Email");
+            c.HasOne(c => c.CorporateCustomer);
+            c.HasOne(c => c.IndividualCustomer);
+            c.HasMany(c => c.Rentals);
+        });
+
         modelBuilder.Entity<Fuel>(f =>
         {
-            f.ToTable("Fuels").HasKey(k => k.Id);
-            f.Property(p => p.Id).HasColumnName("Id");
-            f.Property(p => p.Name).HasColumnName("Name");
-            f.HasMany(p => p.Models);
+            f.ToTable("Fuels").HasKey(f => f.Id);
+            f.Property(f => f.Id).HasColumnName("Id");
+            f.Property(f => f.Name).HasColumnName("Name");
+            f.HasMany(f => f.Models);
         });
+
+        modelBuilder.Entity<IndividualCustomer>(c =>
+        {
+            c.ToTable("IndividualCustomers").HasKey(i => i.Id);
+            c.Property(i => i.Id).HasColumnName("Id");
+            c.Property(i => i.CustomerId).HasColumnName("CustomerId");
+            c.Property(i => i.FirstName).HasColumnName("FirstName");
+            c.Property(i => i.LastName).HasColumnName("LastName");
+            c.Property(i => i.NationalIdentity).HasColumnName("NationalIdentity");
+            c.HasOne(i => i.Customer);
+        });
+
         modelBuilder.Entity<Model>(m =>
         {
             m.ToTable("Models").HasKey(k => k.Id);
@@ -79,16 +115,20 @@ public class BaseDbContext : DbContext
             m.HasOne(p => p.Fuel);
             m.HasOne(p => p.Transmission);
         });
+
         modelBuilder.Entity<Rental>(r =>
         {
             r.ToTable("Rentals").HasKey(k => k.Id);
-            r.Property(p => p.Id).HasColumnName("Id");
-            r.Property(p => p.CarId).HasColumnName("CarId");
-            r.Property(p => p.RentStartDate).HasColumnName("RentStartDate");
-            r.Property(p => p.RentEndDate).HasColumnName("RentEndDate");
-            r.Property(p => p.ReturnDate).HasColumnName("ReturnDate");
-            r.HasOne(p => p.Car);
+            r.Property(r => r.Id).HasColumnName("Id");
+            r.Property(r => r.CustomerId).HasColumnName("CustomerId");
+            r.Property(r => r.CarId).HasColumnName("CarId");
+            r.Property(r => r.RentStartDate).HasColumnName("RentStartDate");
+            r.Property(r => r.RentEndDate).HasColumnName("RentEndDate");
+            r.Property(r => r.ReturnDate).HasColumnName("ReturnDate");
+            r.HasOne(r => r.Car);
+            r.HasOne(r => r.Customer);
         });
+
         modelBuilder.Entity<Transmission>(t =>
         {
             t.ToTable("Transmissions").HasKey(k => k.Id);
@@ -107,13 +147,26 @@ public class BaseDbContext : DbContext
         Color[] colorSeeds = { new(1, "Red"), new(2, "Blue") };
         modelBuilder.Entity<Color>().HasData(colorSeeds);
 
+        CorporateCustomer[] corporateCustomers = { new(1, 2, "Ahmet Çetinkaya", "54154512") };
+        modelBuilder.Entity<CorporateCustomer>().HasData(corporateCustomers);
+
+        Customer[] customers = { new(1, "ahmetcetinkaya7@outlook.com"), new(2, "ahmet@cetinkaya.com") };
+        modelBuilder.Entity<Customer>().HasData(customers);
+
         Fuel[] fuelSeeds = { new(1, "Diesel"), new(2, "Electric") };
         modelBuilder.Entity<Fuel>().HasData(fuelSeeds);
+
+        IndividualCustomer[] individualCustomers = { new(1, 1, "Ahmet", "Çetinkaya", "123123123123") };
+        modelBuilder.Entity<IndividualCustomer>().HasData(individualCustomers);
 
         Model[] modelSeeds = { new(1, 1, 1, 2, "418i", 1000, ""), new(2, 2, 2, 1, "CLA 180D", 600, "") };
         modelBuilder.Entity<Model>().HasData(modelSeeds);
 
-        Rental[] rentalSeeds = { new(1, 2, DateTime.Today, DateTime.Today.AddDays(2), null) };
+        Rental[] rentalSeeds =
+        {
+            new(1, 1, 2, DateTime.Today, DateTime.Today.AddDays(2), null),
+            new(2, 2, 1, DateTime.Today, DateTime.Today.AddDays(2), null)
+        };
         modelBuilder.Entity<Rental>().HasData(rentalSeeds);
 
         Transmission[] transmissionsSeeds = { new(1, "Manuel"), new(2, "Automatic") };
