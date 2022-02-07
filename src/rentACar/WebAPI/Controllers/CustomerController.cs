2dfb24435b9442e08c7db698c49c8843ea95fc44
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-07 12:11:35 +0300

    ✨(webapi, controllers): Customers Controllers
    Add CustomersController
    Add IndividualCustomersController
    Add CorporateCustomersController

---
+++ src/rentACar/WebAPI/Controllers/CustomerController.cs
@@ -0,0 +1,52 @@
+﻿using Application.Features.Customers.Commands.CreateCustomer;
+using Application.Features.Customers.Commands.DeleteCustomer;
+using Application.Features.Customers.Commands.UpdateCustomer;
+using Application.Features.Customers.Models;
+using Application.Features.Customers.Queries.GetByIdCustomer;
+using Application.Features.Customers.Queries.GetListCustomer;
+using Core.Application.Requests;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class CustomersController : BaseController
+{
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetById([FromRoute] GetByIdCustomerQuery getByIdCustomerQuery)
+    {
+        Customer result = await Mediator.Send(getByIdCustomerQuery);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListCustomerQuery getListCustomerQuery = new() { PageRequest = pageRequest };
+        CustomerListModel result = await Mediator.Send(getListCustomerQuery);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateCustomerCommand createCustomerCommand)
+    {
+        Customer result = await Mediator.Send(createCustomerCommand);
+        return Created("", result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] UpdateCustomerCommand updateCustomerCommand)
+    {
+        Customer result = await Mediator.Send(updateCustomerCommand);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteCustomerCommand deleteCustomerCommand)
+    {
+        Customer result = await Mediator.Send(deleteCustomerCommand);
+        return Ok(result);
+    }
+}
---
+++ src/rentACar/WebAPI/Controllers/IndividualCustomerController.cs
@@ -0,0 +1,52 @@
+﻿using Application.Features.IndividualCustomers.Commands.CreateIndividualCustomer;
+using Application.Features.IndividualCustomers.Commands.DeleteIndividualCustomer;
+using Application.Features.IndividualCustomers.Commands.UpdateIndividualCustomer;
+using Application.Features.IndividualCustomers.Models;
+using Application.Features.IndividualCustomers.Queries.GetByIdIndividualCustomer;
+using Application.Features.IndividualCustomers.Queries.GetListIndividualCustomer;
+using Core.Application.Requests;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class IndividualCustomersController : BaseController
+{
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetById([FromRoute] GetByIdIndividualCustomerQuery getByIdIndividualCustomerQuery)
+    {
+        IndividualCustomer result = await Mediator.Send(getByIdIndividualCustomerQuery);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListIndividualCustomerQuery getListIndividualCustomerQuery = new() { PageRequest = pageRequest };
+        IndividualCustomerListModel result = await Mediator.Send(getListIndividualCustomerQuery);
+        return Ok(result);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateIndividualCustomerCommand createIndividualCustomerCommand)
+    {
+        IndividualCustomer result = await Mediator.Send(createIndividualCustomerCommand);
+        return Created("", result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] UpdateIndividualCustomerCommand updateIndividualCustomerCommand)
+    {
+        IndividualCustomer result = await Mediator.Send(updateIndividualCustomerCommand);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteIndividualCustomerCommand deleteIndividualCustomerCommand)
+    {
+        IndividualCustomer result = await Mediator.Send(deleteIndividualCustomerCommand);
+        return Ok(result);
+    }
+}
