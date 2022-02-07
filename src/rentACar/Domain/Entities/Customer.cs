cbef038b603446afb87ac52ea3617a11484aba90
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-07 11:57:12 +0300

    🏷️(domain, entities): Add Customers

---
+++ src/rentACar/Domain/Entities/Customer.cs
@@ -0,0 +1,22 @@
+﻿using Core.Persistence.Repositories;
+
+namespace Domain.Entities;
+
+public class Customer : Entity
+{
+    public string Email { get; set; }
+
+    public virtual CorporateCustomer CorporateCustomer { get; set; }
+    public virtual IndividualCustomer IndividualCustomer { get; set; }
+    public virtual ICollection<Rental> Rentals { get; set; }
+
+    public Customer()
+    {
+        Rentals = new List<Rental>();
+    }
+
+    public Customer(int id, string email) : base(id)
+    {
+        Email = email;
+    }
+}
