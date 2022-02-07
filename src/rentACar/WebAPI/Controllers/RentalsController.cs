ba2eb09b784968d7d44bef2e5bb324fe09b4382a
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-07 15:59:27 +0300

    ✨(webapi): Rental update, delete commands and queries

--- src/rentACar/WebAPI/Controllers/RentalsController.cs
+++ src/rentACar/WebAPI/Controllers/RentalsController.cs
@@ -1,4 +1,10 @@
 ﻿using Application.Features.Rentals.Commands.CreateRental;
+using Application.Features.Rentals.Commands.DeleteRental;
+using Application.Features.Rentals.Commands.UpdateRental;
+using Application.Features.Rentals.Models;
+using Application.Features.Rentals.Queries.GetByIdRental;
+using Application.Features.Rentals.Queries.GetListRental;
+using Core.Application.Requests;
 using Domain.Entities;
 using Microsoft.AspNetCore.Mvc;
 
@@ -8,10 +14,39 @@ namespace WebAPI.Controllers;
 [ApiController]
 public class RentalsController : BaseController
 {
+    [HttpGet("{Id}")]
+    public async Task<IActionResult> GetById([FromRoute] GetByIdRentalQuery getByIdRentalQuery)
+    {
+        Rental result = await Mediator.Send(getByIdRentalQuery);
+        return Ok(result);
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetList([FromQuery] PageRequest pageRequest)
+    {
+        GetListRentalQuery getListRentalQuery = new() { PageRequest = pageRequest };
+        RentalListModel result = await Mediator.Send(getListRentalQuery);
+        return Ok(result);
+    }
+
     [HttpPost]
     public async Task<IActionResult> Add([FromBody] CreateRentalCommand createRentalCommand)
     {
         Rental result = await Mediator.Send(createRentalCommand);
         return Created("", result);
+    }
+
+    [HttpPut]
+    public async Task<IActionResult> Update([FromBody] UpdateRentalCommand updateRentalCommand)
+    {
+        Rental result = await Mediator.Send(updateRentalCommand);
+        return Ok(result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody] DeleteRentalCommand deleteRentalCommand)
+    {
+        Rental result = await Mediator.Send(deleteRentalCommand);
+        return Ok(result);
     }
 }
