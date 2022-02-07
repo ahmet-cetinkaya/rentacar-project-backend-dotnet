1652ba2479cba3c7bb191e80950bb647eac11d2e
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-07 19:16:44 +0300

    ✨(infrastructure, adapters): Fake FindeksCreditRate service adapter

---
+++ src/rentACar/Infrastructure/Adapters/FakeFindeksCreditRateService/FakeFindeksCreditRateServiceAdapter.cs
@@ -0,0 +1,13 @@
+﻿using Application.Services;
+
+namespace Infrastructure.Adapters.FakeFindeksCreditRateService;
+
+public class FakeFindeksCreditRateServiceAdapter : IFindeksCreditRateService
+{
+    public short GetScore(string identityNumber)
+    {
+        Random random = new();
+        short score = Convert.ToInt16(random.Next(1900));
+        return score;
+    }
+}
