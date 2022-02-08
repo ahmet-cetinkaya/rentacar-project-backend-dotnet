f6ad70b0cbe08a9b1105a45b36fe89a86eeb0f28
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-08 11:05:00 +0300

    🗃️(persistence, migrations): Add Invoice Migration

---
+++ src/rentACar/Persistence/Migrations/20220208075415_invoice.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    public partial class invoice : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Invoices",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CustomerId = table.Column<int>(type: "int", nullable: false),
+                    No = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CreatedDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RentalStartDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    RentalEndDate = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    TotalRentalDate = table.Column<short>(type: "smallint", nullable: false),
+                    RentalPrice = table.Column<decimal>(type: "decimal(18,2)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Invoices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Invoices_Customers_CustomerId",
+                        column: x => x.CustomerId,
+                        principalTable: "Customers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Invoices",
+                columns: new[] { "Id", "CreatedDate", "CustomerId", "No", "RentalEndDate", "RentalPrice", "RentalStartDate", "TotalRentalDate" },
+                values: new object[,]
+                {
+                    { 1, new DateTime(2022, 2, 8, 0, 0, 0, 0, DateTimeKind.Local), 1, "123123", new DateTime(2022, 2, 10, 0, 0, 0, 0, DateTimeKind.Local), 1000m, new DateTime(2022, 2, 8, 0, 0, 0, 0, DateTimeKind.Local), (short)2 },
+                    { 2, new DateTime(2022, 2, 8, 0, 0, 0, 0, DateTimeKind.Local), 1, "123123", new DateTime(2022, 2, 10, 0, 0, 0, 0, DateTimeKind.Local), 2000m, new DateTime(2022, 2, 8, 0, 0, 0, 0, DateTimeKind.Local), (short)2 }
+                });
+
+            migrationBuilder.UpdateData(
+                table: "Rentals",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "RentEndDate", "RentStartDate" },
+                values: new object[] { new DateTime(2022, 2, 10, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 8, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Rentals",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "RentEndDate", "RentStartDate" },
+                values: new object[] { new DateTime(2022, 2, 10, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 8, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Invoices_CustomerId",
+                table: "Invoices",
+                column: "CustomerId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Invoices");
+
+            migrationBuilder.UpdateData(
+                table: "Rentals",
+                keyColumn: "Id",
+                keyValue: 1,
+                columns: new[] { "RentEndDate", "RentStartDate" },
+                values: new object[] { new DateTime(2022, 2, 9, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 7, 0, 0, 0, 0, DateTimeKind.Local) });
+
+            migrationBuilder.UpdateData(
+                table: "Rentals",
+                keyColumn: "Id",
+                keyValue: 2,
+                columns: new[] { "RentEndDate", "RentStartDate" },
+                values: new object[] { new DateTime(2022, 2, 9, 0, 0, 0, 0, DateTimeKind.Local), new DateTime(2022, 2, 7, 0, 0, 0, 0, DateTimeKind.Local) });
+        }
+    }
+}
