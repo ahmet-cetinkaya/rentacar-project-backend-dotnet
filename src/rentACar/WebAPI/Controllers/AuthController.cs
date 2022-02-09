bcab31315a2e8f0ec0ee6fa107a95e7618dc69c4
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-09 21:48:39 +0300

    ✨(webapi): Auth controller

---
+++ src/rentACar/WebAPI/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Application.Features.Auths.Commands.Login;
+using Application.Features.Auths.Commands.Register;
+using Core.Security.Dtos;
+using Core.Security.JWT;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class AuthController : BaseController
+{
+    [HttpPost("Login")]
+    public async Task<IActionResult> Login([FromBody] UserForLoginDto userForLoginDto)
+    {
+        LoginCommand loginCommand = new() { UserForLoginDto = userForLoginDto };
+        AccessToken result = await Mediator.Send(loginCommand);
+        return Ok(result);
+    }
+
+    [HttpPost("Register")]
+    public async Task<IActionResult> Register([FromBody] UserForRegisterDto userForRegisterDto)
+    {
+        RegisterCommand registerCommand = new() { UserForRegisterDto = userForRegisterDto };
+        AccessToken result = await Mediator.Send(registerCommand);
+        return Created("", result);
+    }
+}
