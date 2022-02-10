e91647dc551ba5e9ae8e72859f92efc1035bb75f
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-10 12:25:51 +0300

    ✨(core, crosscuttingconcers): Exception middleware

---
+++ src/corePackages/Core.CrossCuttingConcers/Exceptions/BusinessProblemDetails.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace Core.CrossCuttingConcerns.Exceptions;
+
+public class BusinessProblemDetails : ProblemDetails
+{
+    public override string ToString()
+    {
+        return JsonConvert.SerializeObject(this);
+    }
+}
---
+++ src/corePackages/Core.CrossCuttingConcers/Exceptions/ExceptionMiddleware.cs
@@ -0,0 +1,81 @@
+﻿using System.Net;
+using FluentValidation;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Core.CrossCuttingConcerns.Exceptions;
+
+public class ExceptionMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ExceptionMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task Invoke(HttpContext context)
+    {
+        try
+        {
+            await _next(context);
+        }
+        catch (Exception exception)
+        {
+            await HandleExceptionAsync(context, exception);
+        }
+    }
+
+    private Task HandleExceptionAsync(HttpContext context, Exception exception)
+    {
+        context.Response.ContentType = "application/json";
+
+        if (exception.GetType() == typeof(ValidationException)) return CreateValidationException(context, exception);
+        if (exception.GetType() == typeof(BusinessException)) return CreateBusinessException(context, exception);
+        return CreateInternalException(context, exception);
+    }
+
+    private Task CreateBusinessException(HttpContext context, Exception exception)
+    {
+        context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.BadRequest);
+
+        return context.Response.WriteAsync(new BusinessProblemDetails
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Type = "https://example.com/probs/business",
+            Title = "Business exception",
+            Detail = exception.Message,
+            Instance = ""
+        }.ToString());
+    }
+
+    private Task CreateValidationException(HttpContext context, Exception exception)
+    {
+        context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.BadRequest);
+        object errors = ((ValidationException)exception).Errors;
+
+        return context.Response.WriteAsync(new ValidationProblemDetails
+        {
+            Status = StatusCodes.Status400BadRequest,
+            Type = "https://example.com/probs/validation",
+            Title = "Validation error(s)",
+            Detail = "",
+            Instance = "",
+            Errors = errors
+        }.ToString());
+    }
+
+    private Task CreateInternalException(HttpContext context, Exception exception)
+    {
+        context.Response.StatusCode = Convert.ToInt32(HttpStatusCode.InternalServerError);
+
+        return context.Response.WriteAsync(new ProblemDetails
+        {
+            Status = StatusCodes.Status500InternalServerError,
+            Type = "https://example.com/probs/internal",
+            Title = "Internal exception",
+            Detail = exception.Message,
+            Instance = ""
+        }.ToString());
+    }
+}
