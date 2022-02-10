f89d6ac4a0bde9ae88ce277a19ac02d5d9c5ab13
Ahmet Cetinkaya <mail@ahmetcetinkaya.info>
2022-02-10 12:26:31 +0300

    🔧(webapi): Add exception middleware

--- src/rentACar/WebAPI/Program.cs
+++ src/rentACar/WebAPI/Program.cs
@@ -1,4 +1,5 @@
 using Application;
+using Core.CrossCuttingConcerns.Exceptions;
 using Infrastructure;
 using Persistence;
 
@@ -25,6 +26,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.ConfigureCustomExceptionMiddleware();
 
 app.UseAuthorization();
 
